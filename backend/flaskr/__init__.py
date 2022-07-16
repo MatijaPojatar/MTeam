868167428219dc0dab45c00b9d7c183286d612ce
mihailot01 <mihailo.t.001@gmail.com>
2022-07-17 00:53:54 +0200

    events cron working

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -3,13 +3,17 @@ import sqlite3
 import json
 from web3 import Web3
 from web3 import EthereumTesterProvider
+import web3.datastructures as wd
 
 from flask import Flask
 
 provider_url = "https://kovan.infura.io/v3/d535298504ac468eb14672b06e22469a"
 
-abi = json.load(open("./flaskr/abi.json"))
-address = '0xE0fBa4Fc209b4948668006B2bE61711b7f465bAe'
+abiAPY = json.load(open("./flaskr/abi.json"))
+addressAPY = '0xE0fBa4Fc209b4948668006B2bE61711b7f465bAe'
+
+abi = json.load(open("./flaskr/abiM.json"))
+address = '0x5466acb6ea9081E625EbC34a92807f15eF614a6d'
    
 def create_app(test_config=None):
     # create and configure the app
@@ -36,7 +40,8 @@ def create_app(test_config=None):
     db.init_app(app)
 
     from . import cron
-    cron.start(app)
+    cron.startAPY(app)
+    cron.startEvents(app)
 
     # a simple page that says hello
     @app.route('/')
@@ -54,7 +59,7 @@ def create_app(test_config=None):
     def apyInfo2():
         w3 = Web3(Web3.HTTPProvider(provider_url))
         print(w3.isConnected())
-        contract = w3.eth.contract(address = address , abi = abi)
+        contract = w3.eth.contract(address = addressAPY , abi = abiAPY)
         _, liquidityIndex, variableBorrowIndex,currentLiquidityRate, currentVariableBorrowRate,currentStableBorrowRate, _ ,aTokenAddress, stableDebtTokenAddress,variableDebtTokenAddress, _ , _ = contract.functions.getReserveData("0xd0A1E359811322d97991E03f863a0C30C2cF029C").call()
         print(liquidityIndex)
         RAY = 10**27
@@ -72,5 +77,21 @@ def create_app(test_config=None):
         return str(variableBorrowAPY)
         pass
 
+    @app.route('/events')
+    def events():
+        w3 = Web3(Web3.HTTPProvider(provider_url))
+        print(w3.isConnected())
+        contract = w3.eth.contract(address = address , abi = abi)
+        event_filter = contract.events.Deposit.createFilter(fromBlock=0)#"latest")
+        print(contract.functions.totalBalance().call())
+        # print(event_filter.get_all_entries())
+        depositEvent = contract.events.Deposit()
+        for event in event_filter.get_all_entries():
+            user = event['args']['_user']
+            value = event['args']['_value']
+            blockNumber = event['blockNumber']
+            print(user, value, blockNumber)
+        return "ok"
+        pass
 
     return app
--- backend/flaskr/cron.py
+++ backend/flaskr/cron.py
@@ -10,16 +10,20 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 provider_url = "https://kovan.infura.io/v3/d535298504ac468eb14672b06e22469a"
 
-abi = json.load(open("./flaskr/abi.json"))
-address = '0xE0fBa4Fc209b4948668006B2bE61711b7f465bAe'
-#address = '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9'
+
 token_address = '0xd0A1E359811322d97991E03f863a0C30C2cF029C'
 #token_address = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+
+abiAPY = json.load(open("./flaskr/abi.json"))
+addressAPY = '0xE0fBa4Fc209b4948668006B2bE61711b7f465bAe'
+
+abi = json.load(open("./flaskr/abiM.json"))
+address = '0x5466acb6ea9081E625EbC34a92807f15eF614a6d'
 
 def fetchAPY(app):
     w3 = Web3(Web3.HTTPProvider(provider_url))
     print(w3.isConnected())
-    contract = w3.eth.contract(address = address , abi = abi)
+    contract = w3.eth.contract(address = addressAPY , abi = abiAPY)
     _, liquidityIndex, variableBorrowIndex,currentLiquidityRate, currentVariableBorrowRate,currentStableBorrowRate, lastUpdateTimestamp ,aTokenAddress, stableDebtTokenAddress,variableDebtTokenAddress, _ , _ = contract.functions.getReserveData("0xd0A1E359811322d97991E03f863a0C30C2cF029C").call()
     RAY = 10**27
     SECONDS_PER_YEAR = 31536000
@@ -58,13 +62,53 @@ def fetchAPY(app):
             db.get_db().commit()
         return True
 
+def fetchEvents(app):
+    w3 = Web3(Web3.HTTPProvider(provider_url))
+    print(w3.isConnected())
+    contract = w3.eth.contract(address = address , abi = abi)
+    
+    with app.app_context():
+        from . import db
+        curr = db.get_db().cursor()
+        
+        curr.execute("SELECT max(blockNumber) as t FROM 'events'")
+        data = curr.fetchall()
+        lastBlock = 0 if data[0][0] is None else data[0][0]+1 
+        print('lastBlock', lastBlock)
+        event_filter = contract.events.Deposit.createFilter(fromBlock=lastBlock)
+        print(contract.functions.totalBalance().call())
+        for event in event_filter.get_all_entries():
+            user = event['args']['_user']
+            value = event['args']['_value']
+            blockNumber = event['blockNumber']
+            print(user, value, blockNumber)
+        
+            if event['event']=='Deposit':
+                curr.execute(
+                ''' INSERT INTO events(address,deposit,blockNumber)
+                    VALUES (?,?,?)
+                ''', (user,value, blockNumber))
+            if event['event']=='Withdraw':
+                curr.execute(
+                ''' INSERT INTO events(address,withdraw,blockNumber)
+                    VALUES (?,?,?)
+                ''', (user,value, blockNumber))
+            db.get_db().commit()
+        
+        return True
 
 scheduler = BackgroundScheduler()
 
 
-def start(app):
-    scheduler.add_job(func=fetchAPY, args=[app], trigger="interval", seconds=10)
-    scheduler.start()
+def startAPY(app):
+    scheduler.add_job(func=fetchAPY, args=[app], trigger="interval", seconds=60)
+    if not scheduler.running:
+        scheduler.start()
+
+def startEvents(app):
+    scheduler.add_job(func=fetchEvents, args=[app], trigger="interval", seconds=10)
+    if not scheduler.running:
+        scheduler.start()
 
 # Shut down the scheduler when exiting the app
 atexit.register(lambda: scheduler.shutdown())
